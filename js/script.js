66f7d84ac38f17d4b1688c2109378c69a1524dc5
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-04-11 14:51:40 +0200

    added mobile navigation and Sticky navigation

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    // threshold : is how much of the element is reached [0:100%(1)]
+    threshold: 0,
+    // to make it start before the threshold by 80px(the height of the navigation bar)
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
